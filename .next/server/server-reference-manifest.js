ef3250702790cac1d158a47948f7559430c3b16b
Nano Segovia <nanosegovia101@gmail.com>
2026-01-13 00:04:59 +0000

    Fix: remove ignored files and update .gitignore

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lURF6T2vTeVghta6Ool0MAECRgSqQ0vpL0dHUwqsvyU=\"\n}"
--- .next/static/chunks/src_components_map_tsx_a9777ec4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_LeafletMapComponent_tsx_228e76fe._.js",
-  "static/chunks/src_components_map_tsx_5f6caf7a._.js"
-],
-    source: "dynamic"
-});
